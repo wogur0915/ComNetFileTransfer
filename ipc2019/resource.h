ba76ee6e8a24a6a3c370f2fff87cfa04e7b8413b
Gnbrkm41 <ganbarukamo@gmail.com>
2022-10-30 17:55:01 +0900

    Prepare UI

--- ipc2019/resource.h
+++ ipc2019/resource.h
@@ -23,6 +23,9 @@
 #define IDC_CHECK_TOALL                 1006
 #define IDC_COMBO1                      1007
 #define IDC_EDIT4                       1009
+#define IDC_PROGRESS1                   1010
+#define IDC_BUTTON1                     1011
+#define IDC_BUTTON2                     1012
 
 // Next default values for new objects
 // 
@@ -30,7 +33,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
